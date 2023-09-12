afaff7c3d8c65ec71b8c34131356aa97bdafe0b1
Rachel <gggggrea@gmail.com>
2023-09-12 22:45:52 +0100

    Create SparkLine component

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { SparklineComponent, Inject, SparklineTooltip } from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+const SparkLine = ({ id, height, width, data, type, color, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      type={type}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="yval"
+      tooltipSettings={{ visible: true, format: "${x}: data ${yval}", trackLineSettings: { visible: true } }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 };
 
 export default SparkLine;
