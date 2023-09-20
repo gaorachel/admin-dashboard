b1d7682cefe15b2cca56ea9357e33ca2bf1fe9b4
Rachel <gggggrea@gmail.com>
2023-09-21 00:30:29 +0100

    Make buttons work

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdOutlineCancel } from "react-icons/md";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
@@ -8,6 +8,22 @@ import { Button } from ".";
 
 const Cart = () => {
   const { currentColor, isClicked, setIsClicked } = useStateContext();
+  const [data, setData] = useState(
+    cartData.map((item) => {
+      return {
+        ...item,
+        qty: 1,
+      };
+    })
+  );
+
+  const [subTotal, setSubTotal] = useState(0);
+
+  useEffect(() => {
+    const sum = data.reduce((acc, item) => acc + parseFloat(item.price.slice(1)) * item.qty, 0);
+
+    return setSubTotal(sum);
+  }, [data]);
 
   return (
     <div className="bg-half-transparent w-full fixed nav-item top-0 right-0 ">
@@ -23,7 +39,7 @@ const Cart = () => {
             <MdOutlineCancel />
           </button>
         </div>
-        {cartData.map((item, index) => (
+        {data.map((item, index) => (
           <div key={index}>
             <div>
               <div className="flex items-center   leading-8 gap-5 border-b-1 border-color dark:border-gray-600 p-4">
@@ -34,11 +50,29 @@ const Cart = () => {
                   <div className="flex gap-4 mt-2 items-center">
                     <p className="font-semibold text-lg">{item.price}</p>
                     <div className="flex items-center ">
-                      <button className="p-2 bg-gray-300 dark:bg-gray-700 text-gray-900 rounded-md" type="button">
+                      <button
+                        className="p-2 bg-gray-300 dark:bg-gray-700 text-gray-900 rounded-md"
+                        type="button"
+                        onClick={() => {
+                          const updatedData = [...data];
+                          updatedData[index].qty = updatedData[index].qty === 0 ? 0 : updatedData[index].qty - 1;
+
+                          setData(updatedData);
+                        }}
+                      >
                         <AiOutlineMinus />
                       </button>
-                      <p className="p-2 text-black dark:text-white"> 0 </p>
-                      <button className="p-2 bg-gray-300 dark:bg-gray-700 text-gray-900 rounded-md" type="button">
+                      <p className="p-2 text-black dark:text-white"> {item.qty} </p>
+                      <button
+                        className="p-2 bg-gray-300 dark:bg-gray-700 text-gray-900 rounded-md"
+                        type="button"
+                        onClick={() => {
+                          const updatedData = [...data];
+                          updatedData[index].qty += 1;
+
+                          setData(updatedData);
+                        }}
+                      >
                         <AiOutlinePlus />
                       </button>
                     </div>
@@ -50,12 +84,12 @@ const Cart = () => {
         ))}
         <div className="mt-3 mb-3">
           <div className="flex justify-between items-center">
-            <p className="text-gray-500 dark:text-gray-200">Sub Total</p>
-            <p className="font-semibold">$890</p>
+            <p className="text-gray-500 dark:text-gray-400">Sub Total</p>
+            <p className="text-gray-500 dark:text-gray-400"> {`£${subTotal}`} </p>
           </div>
           <div className="flex justify-between items-center mt-3">
-            <p className="text-gray-500 dark:text-gray-200">Total</p>
-            <p className="font-semibold">$890</p>
+            <p className="text-black dark:text-gray-300 font-semibold">Total (+ 20% VAT) </p>
+            <p className="text-black dark:text-gray-300 font-semibold"> {`£${subTotal * 1.2}`} </p>
           </div>
         </div>
         <div className="mt-5">
