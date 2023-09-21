0886fd6b6a34d07ba7e46df06077f7409bdb8b08
Rachel <gggggrea@gmail.com>
2023-09-21 20:16:15 +0100

    Fix bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
   const { currentColor } = useStateContext();
 
   return (
-    <div className="mt-24">
-      <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+    <div className="mt-20">
+      <p className="dark:text-gray-200 text-gray-700 text-center">
         © 2023 Originally Created by
         <a href="https://github.com/adrianhajdin" style={{ color: currentColor }}>
           {" "}
