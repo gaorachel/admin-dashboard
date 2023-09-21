09e0bc09e68a5fa2ab85175d8d6c7a98937d9921
Rachel <gggggrea@gmail.com>
2023-09-21 16:10:40 +0100

    Add box shadow

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -8,7 +8,7 @@ const Chat = () => {
   const { currentColor, isClicked, setIsClicked } = useStateContext();
 
   return (
-    <div className="nav-item absolute right-5 md:right-52 top-16 bg-white dark:bg-gray-900 p-8 rounded-lg w-96">
+    <div className="nav-item absolute right-5 md:right-52 top-16 bg-white dark:bg-gray-900 p-8 rounded-lg w-96 shadow-xl shadow-gray-300 dark:shadow-gray-700">
       <div className="flex justify-between items-center">
         <div className="flex gap-3">
           <p className="font-semibold text-lg dark:text-gray-200">Messages</p>
